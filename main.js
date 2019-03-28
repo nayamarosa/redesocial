7e7752e9d3a6ec2cae36de2a4532cf5ce8e3ccf8
Ana-Biscalchin <anacarolinabis@gmail.com>
2019-03-27 23:25:04 -0300

    login html refatorado, sem css e com firebase funcionando, criei nova html para a página principal

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
+$(document).ready(function(){
+  $("#register").click(function(){
+    let email= $("#email").val();
+    let password = $("#password").val();
+    
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    
+    .then(function(){
+      window.location = "main.html"
+    })
+ 
+    .catch(function(error){
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    });
+  });
+});
+
+
+$("#button-login").click(function(){
+  let email= $("#email").val();
+  let password = $("#password").val();
+  firebase.auth().signInWithEmailAndPassword(email, password)
+   .then(function(){
+      window.location = "main.html"
+    })
+  .catch(function(error) {
+    let errorCode = error.code;
+    let errorMessage = error.message;
+  });
+
+  
+ 
+
+});
+
+
+
+
 
