3b4d2286e642e04d5d8b2f6a4f2229d57b6b92dd
nayamarosa <falecom@nayama.com.br>
2019-04-03 15:39:08 -0300

    login e cadastro no html, css e js

---
+++ login.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $(".btn-login").click(enterLogin);
+});
+
+function enterLogin(event){
+    event.preventDefault();
+    
+    let email = $(".login-email").val();
+    let password = $(".login-password").val();
+    // console.log(email, password);
+    loginUserFirebase(email, password);  
+}
+
+function loginUserFirebase(email, password){
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(response){
+        let userId = response.user.uid;
+        redirectPosts(userId);
+    })
+    .catch(function(error){
+        handleError(error);
+    });
+}
+
+function handleError(error) {
+    var errorMessage = error.message;
+    alert(errorMessage);
+}
+
+function redirectPosts(userId){
+    window.location = "home.html?id=" + userId;
+}
---
+++ register.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $(".btn-create").click(createLogin);
+});
+
+function createLogin(event){
+    event.preventDefault();
+    
+    let email = $(".add-email").val();
+    let password = $(".add-password").val();
+    // console.log(email, password);
+    createUserFirebase(name, email, password);
+  }
+  
+  function createUserFirebase(name, email, password){
+    firebase.auth().createUserWithEmailAndPassword(name, email, password)
+    .then(function(response){
+      let userId = response.user.uid;
+      redirectPosts(userId);
+    })
+    .catch(function(error){
+      handleError(error);
+    });
+  }
+
+  function handleError(error) {
+    var errorMessage = error.message;
+    alert(errorMessage);
+}
+
+function redirectPosts(userId){
+    window.location = "home.html?id=" + userId;
+}
