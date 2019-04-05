cdca7198c0da180ff0a571e5d17dab1e9b4e0000
Ana Carolina Silva Biscalchin <anacarolina@gmail.com>
2019-04-05 14:14:09 -0300

    persistencia de autenticação em andamento

--- login.js
+++ login.js
@@ -31,4 +31,26 @@ function redirectPosts(userId){
     window.location = "feed.html?id=" + userId;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//persistência da autenticação( quando o usuário aciona a caixa de lembrar login)//
+
+
+
+
+${"#checkbox-reminder"}.checkbox
   
