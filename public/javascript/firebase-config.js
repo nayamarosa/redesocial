3a5d8bd4a9d88e47549cdb5aca8f6aaefe5996ba
nayamarosa <falecom@nayama.com.br>
2019-04-05 19:54:39 -0300

    realocando arquivo firebase-config

---
+++ public/javascript/firebase-config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyBxQLxwyLDkyKpfReJmRZZwBudnJZQ8W5U",
+  authDomain: "trocaria-11.firebaseapp.com",
+  databaseURL: "https://trocaria-11.firebaseio.com",
+  projectId: "trocaria-11",
+  storageBucket: "trocaria-11.appspot.com",
+  messagingSenderId: "251335613869"
+};
+firebase.initializeApp(config);
