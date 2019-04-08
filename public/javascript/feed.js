86dd5791d23e89965bc04ff1af618ec25748f0de
ana-biscalchin <anacarolinabis@gmail.com>
2019-04-08 17:25:24 -0300

    contador de favoritos implementado

--- public/javascript/feed.js
+++ public/javascript/feed.js
@@ -11,14 +11,16 @@ $(document).ready(function(){
     let newPost = $(".posts-input").val();
     $(".posts-input").val("");
     let postsFromDB = addPoststoDB(newPost);
-    createPostList(postsFromDB.key, newPost);
+    let favInitial = 0;
+    createPostList(postsFromDB.key, newPost, favInitial);
 
     $('#add-post-modal').modal('hide')
   }
   
   function addPoststoDB(text){
     return database.ref("/posts/" + USER_ID).push({
-      text: text
+      text: text,
+      fav:0
     });
   }
   
@@ -28,23 +30,25 @@ $(document).ready(function(){
       snapshot.forEach(function(childSnapshot) {
         let childKey = childSnapshot.key;
         let childData = childSnapshot.val();
-        
-        createPostList(childKey, childData.text);
+               
+        createPostList(childKey, childData.text, childData.fav);
       });
     });
   }
   
-  function createPostList(key, text) {
+  function createPostList(key, text, fav) {
     $(".posts-list").append(`
     <li>
     <span>${text}</span>
     <i data-toggle="modal" class="update-modal" data-id=${key} data-target="#update-post-modal">EDITAR</i>
     <i data-toggle="modal" class="remove-modal" data-id=${key} data-target="#remove-post-modal">EXCLUIR</i>
-    <i data-toggle="modal" class="update-modal" data-id=${key} data-target="#fav-post-modal">FAVORITAR</i>
+    <i data-toggle="modal" class="favorite-modal" data-id=${key} data-target="#favorite-post-modal">FAVORITAR</i>
+    <i data-toggle="modal" class="favorite-count-modal" data-id=${key} data-target="#favorite-count-modal"> ${fav}</i>
     </li> `);
     
     removePosts();
     updatePosts();
+    favoriteCount(key);
   }
 
   $("#button-logout").click(signOut);
