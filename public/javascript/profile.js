3e3a485d80b5f6b71b21e2d5ce14f077d4c13ec2
Ana-Biscalchin <anacarolinabis@gmail.com>
2019-04-11 04:52:30 -0300

    implementando upload de avatar

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -2,18 +2,17 @@
 $(document).ready(function showProfile() { 
     
 
-$("#input-b9").on("change", function(event) {
-selectedFile = event.target.files[0];
-})
+// $("#profilePicture").on("change", function(event) {
+// selectedFile = event.target.files[0];
+// })
 
-// function fileInput() {
-//     $("#input-b9").fileinput({
-//         showPreview: false,
-//         showUpload: false,
-//         elErrorContainer: '#kartik-file-errors',
-//         allowedFileExtensions: ["jpg", "png", "gif"]
-//         //uploadUrl: '/site/file-upload-single'
-//     });
+
+    $("#profilePicture").fileinput({
+        showPreview: false,
+        showUpload: false,
+        elErrorContainer: '#kartik-file-errors',
+        allowedFileExtensions: ["jpg", "png", "gif"]
+        //uploadUrl: '/site/file-upload-single'
 
 function uploadFile() {
   let storageRef = firebase.storage().ref('/profilePicture/'+ filename); 
